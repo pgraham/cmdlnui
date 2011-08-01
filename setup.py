4153078fffbd4b0760a652648e7384897c443f82
Philip Graham <philip@zeptech.ca>
2011-08-01 11:14:21 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+setup(
+    name = 'Command line UI',
+    packages = ['cmdlnui'],
+    version='0.1',
+    description = "Command Line UI Builder",
+    author = "Philip Graham",
+    author_email = "philip@zeptech.ca",
+    url = "https://github.com/pgraham/cmdlnui",
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Software Development :: User Interfaces"
+    ],
+    long_description = """\
+Command Line UI Utility
+-----------------------
+
+Aids in building command line interfaces for Python programs.
+"""
+)
